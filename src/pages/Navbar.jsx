1f8467d0a40ecf6161fbfa0306870e91a4030950
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2024-10-27 15:12:52 +0530

    Navbar bug fixed

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <nav className='h-16 w-full bg-white z-10 flex justify-between items-end containerP pb-3 text-[1.2rem] fixed top-10'>
+        <nav className='h-16 w-full bg-white z-10 flex justify-between items-end containerP pb-3 text-[1.2rem] fixed top-6'>
             <Link to='/' onClick={() => window.scrollTo(0, 0)} className="home hover:underline cursor-pointer hover:opacity-90"><i className="fa-solid fa-house"></i> Home</Link>
             <div className="flex gap-5">
                 <Link to='/wishlist' onClick={() => window.scrollTo(0, 0)} className="wishList hover:underline cursor-pointer hover:opacity-90"><i className="fa-solid fa-heart"></i> Wishlist</Link>
